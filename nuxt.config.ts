5bb9b8a131bbd8e8e899cfa33e0fdf88bc57893a
youngwen <i@youngwen.com>
2024-07-31 17:40:40 +0800

    ppp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
     '/api/**': {cors: true},
   },
+
+  modules: ['nuxt-security'],
+
+  security: {
+    headers: false,
+    xssValidator: false
+  },
+
+  compatibilityDate: '2024-07-31',
 });
